ae9fb89082fcc61a8942f9bb53b40935e6b72f46
amnhh <2570079254@qq.com>
2017-04-13 13:18:28 +0800

    in node way to build

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+/**
+ * Author : Amnhh
+ * Email : amnhhlod@gmail.com
+ * Date : 2017/4/13
+ */
+
+var path = require('path');
+
+module.exports = {
+  // 入口
+  entry : './app/index.js',
+  output : {
+    // 打包的文件名
+    filename : 'boudle2.js',
+    // 打包的文件存放路径
+    path : path.resolve(__dirname, './dist2')
+  }
+};
