e0eb741aa8e03a5885927ec70283a47c05560e0d
amnhh <2570079254@qq.com>
2017-04-13 13:24:38 +0800

    update the note and practice of build declared in pkg with the mixed path

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -8,7 +8,10 @@ var path = require('path');
 
 module.exports = {
   // 入口
-  entry : './../app/index.js',
+  // 如果在外部，比如我在外部 `pkg` 里面定义指令 : `build1` => "webpack --config build/webpack.config.js"
+  // 我在这个 `config` 里面的所有的东西，都会以命令定义的文件层级来确定，也就是以 `pkg` 所在的地址来确定,
+  // 而不是以 `config` 文件所在的 `pwd` 来确定
+  entry : './app/index.js',
   output : {
     // 打包的文件名
     filename : 'boudle1.js',
