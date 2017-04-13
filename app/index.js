e2a47056649502347a9d5f623596dadd00492f51
amnhh <2570079254@qq.com>
2017-04-13 13:11:12 +0800

    update demo

--- app/index.js
+++ app/index.js
@@ -3,3 +3,14 @@
  * Email : amnhhlod@gmail.com
  * Date : 2017/4/13
  */
+import _ from 'lodash';
+function component () {
+  var element = document.createElement('div');
+
+  /* lodash is required for the next line to work */
+  element.innerHTML = _.join(['Hello','webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
